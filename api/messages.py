f73f9b3454b481e2b35c24619d7aa1d485314c4c
Aadhav Rajesh <aadhav.rajesh@gmail.com>
2026-04-05 12:52:33 -0700

    trying again

--- api/messages.py
+++ api/messages.py
@@ -7,17 +7,13 @@ API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
 
 class handler(BaseHTTPRequestHandler):
 
+    def do_GET(self):
+        self._respond(200, b'{"ok":true}')
+
     def do_OPTIONS(self):
         self.send_response(200)
         self._cors()
         self.end_headers()
-
-    def do_GET(self):
-        self.send_response(200)
-        self._cors()
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
-        self.wfile.write(b'{"ok":true}')
 
     def do_POST(self):
         length = int(self.headers.get("Content-Length", 0))
@@ -51,5 +47,5 @@ class handler(BaseHTTPRequestHandler):
 
     def _cors(self):
         self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
